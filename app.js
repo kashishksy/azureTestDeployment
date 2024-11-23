148d0ca33d9fd942d728e9f67b8bfe1672fd3e83
kashishksy <kashishksy2112@gmail.com>
2024-11-23 09:55:22 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req,res) => {
+
+    res.send('Hello World')
+
+});
+
+app.get('/test', (req,res) => {
+ 
+    res.send("Hello world test")
+
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+
+    console.log(`Server is running on port ${PORT}`)
+})
